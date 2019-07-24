94fee0a5f1e8b94659184410c57c8c9a40782f45
Kyrylo Biloborodko <kbeloborodko@gmail.com>
2019-07-24 14:50:39 +0300

    Update votes endpoint

--- server/src/server.js
+++ server/src/server.js
@@ -12,32 +12,46 @@ app.get('/api/articles/:name', async (req, res) => {
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
     const db = client.db('blog');
 
-    const articleInfo = await db.collection('articles').findOne({
-      name: articleName
-    });
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
 
     res.status(200).json(articleInfo);
 
     client.close();
   }
   catch (error) {
-    res.status(500).json({ message: 'Error connecting to db', error})
+    res.status(500).json({ message: 'Error connecting to db', error })
   }
 });
 
-app.post('/api/articles/:name/votes', (req, res) => {
-  const articleName = req.params.name;
+app.post('/api/articles/:name/votes', async (req, res) => {
+  try {
+    const articleName = req.params.name;
 
-  articlesInfo[articleName].votes += 1;
+    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
+    const db = client.db('blog');
 
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].votes} votes!`);
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne({ name: articleName }, {
+      '$set': {
+        votes: articleInfo.votes + 1
+      }
+    });
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+
+    res.status(200).json(updatedArticleInfo);
+
+    client.close();
+  }
+  catch (error) {
+    res.status(500).json({ message: 'Error connecting to db', error })
+  }
 });
 
 app.post('/api/articles/:name/comments', (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
 
-  articlesInfo[articleName].comments.push({username, text});
+  articlesInfo[articleName].comments.push({ username, text });
 
   res.status(200).send(articlesInfo[articleName]);
 });
