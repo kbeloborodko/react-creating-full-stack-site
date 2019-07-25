ebf2a0e4528be7de780fda9bcf449186ee867dab
Kyrylo Biloborodko <kbeloborodko@gmail.com>
2019-07-25 16:17:05 +0300

    Enhance ArticlePage component

---
+++ client/src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loader = () => (
+  <div className="d-flex align-items-center">
+    <strong>Loading...</strong>
+    <div className="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+  </div>
+);
+
+export default Loader;
--- client/src/pages/ArticlePage.js
+++ client/src/pages/ArticlePage.js
@@ -1,46 +1,81 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
 import NotFoundPage from './NotFoundPage';
+import Loader from '../components/Loader';
 import CommentsList from '../components/CommentsList';
-import articleContent from './article-content';
 import ArticlesList from '../components/ArticlesList';
 
-const ArticlePage = ({ match }) => {
-  const name = match.params.name;
-  const relatedArticles = articleContent.filter(article => article.name !== name);
+class ArticlePage extends Component {
+  constructor(props) {
+    super(props);
 
-  const [articleInfo, setArticleInfo] = useState({
-    name: '',
-    content: [],
-    comments: [],
-    votes: 0
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(`/api/articles/${name}`);
-      const body = await result.json();
-
-      setArticleInfo(body);
-    };
-
-    fetchData();
-  }, [name]);
-
-  if (!articleInfo) {
-    return <NotFoundPage />;
+    this.state = {
+      articleInfo: {
+        title: '',
+        content: [],
+        comments: [],
+        relatedArticles: []
+      },
+      articleNotFound: false,
+      loading: false,
+      relatedArticles: []
+    }
   }
-  return (
-    <>
-      <h1>{articleInfo.name}</h1>
-      <p>This post has been voted {articleInfo.votes} times.</p>
-      {articleInfo.content.map((paragraph, key) => (
-        <p key={key}>{paragraph}</p>
-      ))}
-      <CommentsList comments={articleInfo.comments} />
-      <h2 className="h3">Related Articles</h2>
-      <ArticlesList articles={relatedArticles} gridView={true} />
-    </>
-  )
-};
 
-export default ArticlePage;
+  componentDidMount() {
+    const { name } = this.props.match.params;
+
+    this.setState({ loading: true });
+
+    fetch(`/api/articles`)
+      .then(response => response.json())
+      .then(data => {
+        const articleInfo = data.filter(article => article.name === name);
+
+        if (!articleInfo.length) {
+          this.setState({
+            articleNotFound: true
+          });
+
+          return;
+        }
+
+        const relatedArticles = data.filter(article => article.name !== name);
+
+        this.setState({
+          articleInfo: articleInfo[0],
+          loading: false,
+          relatedArticles,
+        });
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const { articleInfo, articleNotFound, loading, relatedArticles } = this.state;
+
+    if (articleNotFound) {
+      return <NotFoundPage />;
+    }
+
+    if (loading) {
+      return <Loader />;
+    }
+
+    return (
+      <>
+        <h1>{articleInfo.title}</h1>
+        <p>This post has been voted {articleInfo.votes} times.</p>
+        {articleInfo.content.map((paragraph, key) => (
+          <p key={key}>{paragraph}</p>
+        ))}
+        <CommentsList comments={articleInfo.comments} />
+        <h2 className="h3">Related Articles</h2>
+        <ArticlesList articles={relatedArticles} gridView={true} />
+      </>
+    );
+  }
+}
+
+export default withRouter(ArticlePage);
