cad0af39746035f12a17fb5616f04fea14b94210
Kyrylo Biloborodko <kbeloborodko@gmail.com>
2019-07-26 12:22:11 +0300

    Prepare app for deployment

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import { MongoClient } from 'mongodb';
+import path from 'path';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/build')));
 app.use(bodyParser.json());
 
 const withDB = async (operations, res) => {
@@ -92,6 +94,10 @@ app.post('/api/articles/:name/comments', (req, res) => {
 
     res.status(200).send(updatedArticleInfo);
   }, res);
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
 app.listen(8000, () => {
