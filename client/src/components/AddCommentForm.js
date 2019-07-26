8a3059862b35e9379d7bbf7c50947968b2fadff0
Kyrylo Biloborodko <kbeloborodko@gmail.com>
2019-07-26 11:59:04 +0300

    Small enhancements to existing components

--- client/src/components/AddCommentForm.js
+++ client/src/components/AddCommentForm.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddCommentForm = ({ articleName, updateCommentsList }) => {
-  const [username, setUserName] = useState('');
-  const [commentText, setCommentText] = useState('');
+  const [username, setUserName] = useState("");
+  const [commentText, setCommentText] = useState("");
 
   const addComment = async () => {
     const result = await fetch(`/api/articles/${articleName}/comments`, {
-      method: 'post',
-      body: JSON.stringify({  username, text: commentText }),
+      method: "post",
+      body: JSON.stringify({ username, text: commentText }),
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json"
       }
     });
 
@@ -17,8 +17,8 @@ const AddCommentForm = ({ articleName, updateCommentsList }) => {
 
     updateCommentsList(body.comments);
 
-    setUserName('');
-    setCommentText('');
+    setUserName("");
+    setCommentText("");
   };
 
   return (
@@ -26,13 +26,27 @@ const AddCommentForm = ({ articleName, updateCommentsList }) => {
       <h3>Add Your Comment</h3>
       <div className="form-group">
         <label htmlFor="user-name">Your Name:</label>
-        <input type="text" id="user-name" className="form-control" value={username} onChange={(e) => setUserName(e.target.value)} />
+        <input
+          type="text"
+          id="user-name"
+          className="form-control"
+          value={username}
+          onChange={e => setUserName(e.target.value)}
+        />
       </div>
       <div className="form-group">
         <label htmlFor="comment-text">Your Comment:</label>
-        <textarea id="comment-text" rows="3" className="form-control" value={commentText} onChange={(e) => setCommentText(e.target.value)} />
+        <textarea
+          id="comment-text"
+          rows="3"
+          className="form-control"
+          value={commentText}
+          onChange={e => setCommentText(e.target.value)}
+        />
       </div>
-      <button className="btn btn-info" onClick={() => addComment()}>Add</button>
+      <button className="btn btn-info" onClick={() => addComment()}>
+        Add Comment
+      </button>
     </div>
   );
 };
--- client/src/components/Article.js
+++ client/src/components/Article.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const renderArticleContent = (article, gridView) => {
   if (gridView) {
-    return (
-      <p>{article.excerpt ? article.excerpt : null}</p>
-    );
-  }
-  else {
-    return (
-      <p>{article.content.length ? article.content[0] : null}</p>
-    );
+    return <p>{article.excerpt ? article.excerpt : null}</p>;
+  } else {
+    return <p>{article.content.length ? article.content[0] : null}</p>;
   }
 };
 
 const Article = ({ article, gridView }) => (
   <div className="media mb-4">
     <div className="media-body">
-      {gridView ? <h3 className="h4">{article.title}</h3> : <h2>{article.title}</h2>}
+      {gridView ? (
+        <h3 className="h4">{article.title}</h3>
+      ) : (
+        <h2>{article.title}</h2>
+      )}
       {renderArticleContent(article, gridView)}
-      <Link className="btn btn-info" to={`/article-page/${article.name}`}>Read More</Link>
+      <Link className="btn btn-info" to={`/article-page/${article.name}`}>
+        Read More...
+      </Link>
     </div>
   </div>
 );
